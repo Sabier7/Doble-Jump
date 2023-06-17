82bb06ff0b11877e1a6389a28c871d7b4cc59065
Jonatan David Vargas Revollo <47402269+Sabier7@users.noreply.github.com>
2023-06-17 13:58:53 +0200

    Add files via upload

---
+++ project/android-studio-3.5/app/build/generated/source/buildConfig/androidTest/debug/edu/esne/basicsexample/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.esne.basicsexample.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.esne.basicsexample.test";
+  public static final String BUILD_TYPE = "debug";
+}
